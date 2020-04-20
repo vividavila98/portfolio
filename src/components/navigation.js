19f6d9368a2bf5bdfa68b5f0efabf7cedd7dd5c5
vividavila98 <vdavila@ucsd.edu>
2020-04-19 20:44:45 -0700

    fixes

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,6 +6,7 @@ import {
 } from 'reactstrap';
 import { NavHashLink } from 'react-router-hash-link';
 import menu from '../images/menu.svg';
+import resume from '../images/Viviana_Davila_Resume.pdf';
 
 export default function Navigation() {
     const [isOpen, setIsOpen] = useState(false);
@@ -25,16 +26,13 @@ export default function Navigation() {
                           <NavHashLink to='/#about'><span className='code'>01. </span>About</NavHashLink>
                       </NavItem>
                       <NavItem>
-                          <NavHashLink to='/#experience'><span className='code'>02. </span>Experience</NavHashLink>
+                          <NavHashLink to='/#projects'><span className='code'>02. </span>Projects</NavHashLink>
                       </NavItem>
                       <NavItem>
-                          <NavHashLink to='/#projects'><span className='code'>03. </span>Projects</NavHashLink>
+                          <NavHashLink to='/#contact'><span className='code'>03. </span>Contact</NavHashLink>
                       </NavItem>
                       <NavItem>
-                          <NavHashLink to='/#contact'><span className='code'>04. </span>Contact</NavHashLink>
-                      </NavItem>
-                      <NavItem>
-                          <Button className='contact-btn'><a href='#'>Resume</a></Button>
+                          <Button className='contact-btn'><a href={resume} target='_blank'>Resume</a></Button>
                       </NavItem>
                   </Nav>
               </Collapse>
